42b56ce21c101bbc3752b785beb129afdcf309f7
Kirill Efimov <kirill@snyk.io>
2021-10-18 13:00:22 +0300

    disableDefaultTyping-enableDefaultTyping

--- src/main/java/org/some/company/controller/AppController.java
+++ src/main/java/org/some/company/controller/AppController.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.*;
 import org.some.company.model.Org;
-import org.some.company.model.User;
 
 import java.io.IOException;
 
@@ -17,18 +16,6 @@ public class AppController {
         return "Hello Michael!";
     }
 
-    // Vulnerable because User class uses JsonTypeInfo annotation and has field of type Object
-    @Post(value = "/user", consumes = MediaType.APPLICATION_JSON)
-    public void addUser(@Body String payload) throws IOException {
-        System.out.println("Received add user request with payload: " + payload);
-
-        ObjectMapper om = new ObjectMapper();
-        User newUser = om.readValue(payload, User.class);
-
-        System.out.println("Added new user with name:" + newUser.name +
-                " and email " + newUser.email);
-    }
-
     // Vulnerable because Org class has field of type Object and is deserialized with
     // ObjectMapper with enabled polymorphic type handling (enableDefaultTyping)
     @Post(value = "/org", consumes = MediaType.APPLICATION_JSON)
@@ -36,6 +23,7 @@ public class AppController {
         System.out.println("Received add organization request with payload: " + payload);
 
         ObjectMapper om = new ObjectMapper();
+        om.disableDefaultTyping();
         om.enableDefaultTyping();
 
         Org newOrg = om.readValue(payload, Org.class);
